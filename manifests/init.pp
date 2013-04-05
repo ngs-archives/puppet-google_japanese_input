131521d17340eb707dc01e8908f8123e26286aa5
Atsushi Naganse <a@ngs.io>
2013-04-05 10:33:47 +0900

    update provider

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class google_japanese_input {
   package { 'google_japanese_input':
     source   => 'http://dl.google.com/dl/japanese-ime/1.8.1310.1/googlejapaneseinput.dmg',
-    provider => 'appdmg'
+    provider => 'pkgdmg'
   }
 }
